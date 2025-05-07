39aec315e2b5beb5d324240f234f6bc72b01a3c4
MarceloSilvestre <marcelohs.silva72@gmail.com>
2025-05-07 19:45:19 -0300

    melhoria do codigo base e mudança de if else para switch case

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,75 +1,57 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
 
         //esse programa serve para informar o valor que um professor ficticio ganha, de acordo com a sua modalidade de trabalho sejam elas horista,clt ou pj
-
-        // Criação de um novo scanner para ler entradas do teclado, que será utilizado ao longo de todo o programa
+        
         Scanner teclado = new Scanner(System.in);
 
-        // Solicitação do nome do professor
-        System.out.println("Digite seu nome ");
-        String nome;
-        nome = teclado.next();
+        try {
+            System.out.println("Digite seu nome: ");
+            String nome = teclado.next();
 
-        // escolha do tipo de vínculo empregatício
-        System.out.println("Digite que tipo de professor você é digite: 1 se é um professor horista, 2 se é um professor clt ou 3 se é um professor pj");
-        int TipoDeProf;
-        TipoDeProf = teclado.nextInt();
+            System.out.println("Digite o número equivalente ao tipo de professor você é:");
+            System.out.println("1 - Horista");
+            System.out.println("2 - CLT");
+            System.out.println("3 - PJ");
+            int TipoDeProf = teclado.nextInt();
 
-        // Declaração de constantes
-        int HORISTA;
-        int CLT;
-        int PJ;
+            switch (TipoDeProf) {
+                case 1:
+                    System.out.println("Digite o número de horas trabalhadas:");
+                    double horasTrabalhadas = teclado.nextDouble();
 
-        HORISTA =1;
-        CLT = 2;
-        PJ = 3;
+                    System.out.println("Digite o valor recebido por hora de trabalho:");
+                    double valorHora = teclado.nextDouble();
 
-        // Lógica de decisão
-        if(TipoDeProf == CLT){
-            //professor clt
-            System.out.println("digite seu salário mensal ");
-            double salarioCLT;
-            salarioCLT = teclado.nextDouble();
-            System.out.print("O professor ");
-            System.out.print(nome);
-            System.out.print(" recebe um salário de R$ ");
-            System.out.print(salarioCLT);
-            System.out.print(" por mês");
+                    double salarioHorista = horasTrabalhadas * valorHora;
+                    System.out.println("O professor " + nome + " recebe um salário de R$ " + salarioHorista + " por mês");
+                    break;
+
+                case 2:
+                    System.out.println("Digite seu salário mensal:");
+                    double salarioCLT = teclado.nextDouble();
+                    System.out.println("O professor " + nome + " recebe um salário de R$ " + salarioCLT + " por mês");
+                    break;
+
+                case 3:
+                    System.out.println("Digite o valor do seu contrato com a instituição:");
+                    double salarioPJ = teclado.nextDouble();
+                    System.out.println("O professor " + nome + " recebe um salário de R$ " + salarioPJ + " por mês");
+                    break;
+
+                default:
+                    System.out.println("Erro: Tipo de professor inválido.");
+                    break;
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Entrada inválida. Certifique-se de digitar números corretamente.");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
         }
-        else if(TipoDeProf == HORISTA){
-            //professor horista
-            System.out.println("digite o número de horas trabalhadas");
-            Double horasTrabalhadas;
-            horasTrabalhadas = teclado.nextDouble();
-            System.out.println("digite o valor recebido por hora de trabalho");
-            double valorHora;
-            valorHora = teclado.nextDouble();
-            double salarioHorista = horasTrabalhadas * valorHora;
-            System.out.print("O professor ");
-            System.out.print(nome);
-            System.out.print(" recebe um salário de R$ ");
-            System.out.print(salarioHorista);
-            System.out.print(" por mês");
-        }
-        else if(TipoDeProf == PJ){
-            //professor pj
-            System.out.println("digite o valor do seu contrato com a institução");
-            double salarioPJ;
-            salarioPJ = teclado.nextDouble();
-            System.out.print("O professor ");
-            System.out.print(nome);
-            System.out.print(" recebe um salário de R$ ");
-            System.out.print(salarioPJ);
-            System.out.print(" por mês");
-        }
-        else {
-            // Mensagem de erro
-            System.out.println("este número não é equivalente a um tipo de professor");
-        }
-        //fechando o teclado
-        teclado.close();
+            teclado.close();
     }
 }
